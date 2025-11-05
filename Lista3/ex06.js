ba63c3e331b43927e40e9963aa768d7edad2eb11
Gabriel Freitas <128260611+GabrielFHE@users.noreply.github.com>
2025-11-05 11:57:28 -0300

    Update ex06.js

--- Lista3/ex06.js
+++ Lista3/ex06.js
@@ -9,6 +9,7 @@ function estoqueTotal(estoque){
   const valorProdutos = produtosEstoque.reduce((total, produto)=>{
     return total + (produto.preco * produto.quantidade)
   }, 0)
-  p.textContent = `Valor total do estoque (produtos com mais de 5 unidades)` ; `R$ ${valorProdutos}`
+  p.textContent = `Valor total do estoque (produtos com mais de 5 unidades) : R$ ${valorProdutos}`
   document.body.appendChild(p)
+
 }
